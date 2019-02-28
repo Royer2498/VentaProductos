4ea98c7978ae7c0ddc946376e0a08cad6f3f15ce
Royer2498 <royer_2498@hotmail.com>
2019-02-27 21:55:50 -0400

    Inicio del proyecto

---
+++ test/VentaProductos_test.js
@@ -0,0 +1,11 @@
+var assert = require('assert');
+var expect = require('chai').expect;
+var should = require('chai').should();
+
+
+describe('VentaProductosTest',function () {
+
+    it('Deberia pasar la prueba',function () {
+        expect(true).equal(true);
+    });
+});
